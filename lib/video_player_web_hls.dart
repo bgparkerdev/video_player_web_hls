919c33689274460722e98682818c44186c586143
Brent Parker <bgparker1986@gmail.com>
2021-09-12 16:28:24 -0700

    turn off autoplay

--- lib/video_player_web_hls.dart
+++ lib/video_player_web_hls.dart
@@ -200,6 +200,9 @@ class _VideoPlayer {
     // Allows Safari iOS to play the video inline
     videoElement.setAttribute('playsinline', 'true');
 
+    // Set autoplay to false since most browsers won't autoplay a video unless it is muted
+    videoElement.setAttribute('autoplay', 'false');
+
     ui.platformViewRegistry.registerViewFactory(
         'videoPlayer-$textureId', (int viewId) => videoElement);
 
